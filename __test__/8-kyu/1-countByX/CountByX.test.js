cfa35f133f263b094b873e598040272101ce2660
znakarik <znakarik@icloud.com>
2023-10-08 00:19:47 +0300

    formatted code

---
+++ __test__/8-kyu/1-countByX/CountByX.test.js
@@ -0,0 +1,10 @@
+const {deepEqual} = require("assert");
+const {countBy} = require("../../../src/8-kyu/1-countByX/CountByX");
+
+
+describe("Basic Tests", function () {
+    it("Testing for fixed tests", () => {
+        deepEqual(countBy(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "Array does not match")
+        deepEqual(countBy(2, 5), [2, 4, 6, 8, 10], "Array does not match")
+    });
+});
